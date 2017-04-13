2dd1c6ef60df3c5149045500c23aa3f6964196a2
SpoooonBK <erodriguez927@gmail.com>
2017-04-13 16:08:42 -0400

    Widget added

--- app/src/main/java/com/udacity/stockhawk/widget/StockHawkRemoteViewsService.java
+++ app/src/main/java/com/udacity/stockhawk/widget/StockHawkRemoteViewsService.java
@@ -4,18 +4,25 @@ import android.app.LoaderManager;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Binder;
 import android.os.Bundle;
 import android.support.v4.content.CursorLoader;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
 import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.data.Contract;
+import com.udacity.stockhawk.data.PrefUtils;
 import com.udacity.stockhawk.model.StockData;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import timber.log.Timber;
 
@@ -32,7 +39,12 @@ public class StockHawkRemoteViewsService extends RemoteViewsService{
         return new RemoteViewsFactory() {
 
 
-            ArrayList<StockData> mStockData;
+            private Cursor data = null;
+            private DecimalFormat dollarFormatWithPlus;
+            private DecimalFormat dollarFormat;
+            private DecimalFormat percentageFormat;
+
+
 
 
             @Override
@@ -46,12 +58,26 @@ public class StockHawkRemoteViewsService extends RemoteViewsService{
             @Override
             public void onDataSetChanged() {
 
-                mStockData = intent.getParcelableArrayListExtra(StockHawkWidgetIntentService.STOCK_DATA);
+                if (data != null) {
+                    data.close();
+                }
+
 
                 Timber.v("FACTORY ON DATA SET CHANGED");
-
                 final long identityToken = Binder.clearCallingIdentity();
+
+
+
+                data = getContentResolver().query(
+                        Contract.Quote.URI,
+                        null,
+                        null,
+                        null,
+                        null
+                );
+
                 Binder.restoreCallingIdentity(identityToken);
+
 
             }
 
@@ -62,18 +88,58 @@ public class StockHawkRemoteViewsService extends RemoteViewsService{
 
             @Override
             public int getCount() {
-                return mStockData == null ? 0 : mStockData.size();
+                return data == null ? 0 : data.getCount();
             }
 
             @Override
             public RemoteViews getViewAt(int position) {
 
-                StockData stockData = mStockData.get(position);
+                if (position == AdapterView.INVALID_POSITION ||
+                        data == null || !data.moveToPosition(position)) {
+                    return null;
+                }
+
+                dollarFormat = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+                dollarFormatWithPlus = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+                dollarFormatWithPlus.setPositivePrefix("+$");
+                percentageFormat = (DecimalFormat) NumberFormat.getPercentInstance(Locale.getDefault());
+                percentageFormat.setMaximumFractionDigits(2);
+                percentageFormat.setMinimumFractionDigits(2);
+                percentageFormat.setPositivePrefix("+");
+
+                String symbol = data.getString(data.getColumnIndex(Contract.Quote.COLUMN_SYMBOL));
+
+
+                float rawAbsoluteChange = data.getFloat(Contract.Quote.POSITION_ABSOLUTE_CHANGE);
+                float percentageChange = data.getFloat(Contract.Quote.POSITION_PERCENTAGE_CHANGE);
+                float rawPrice = data.getFloat(Contract.Quote.POSITION_PRICE);
+
+
+                String price = dollarFormat.format(rawPrice);
+                String change = dollarFormatWithPlus.format(rawAbsoluteChange);
+                String percentage = percentageFormat.format(percentageChange / 100);
+
 
                 RemoteViews views = new RemoteViews(getPackageName(), R.layout.widget_item);
-                views.setTextViewText(R.id.widget_symbol, stockData.getSymbol());
-                views.setTextViewText(R.id.widget_current_price, stockData.getPrice().toString());
-                views.setTextViewText(R.id.widget_price_change, stockData.getPercentageChange().toString());
+                views.setTextViewText(R.id.widget_symbol, symbol);
+                views.setTextViewText(R.id.widget_current_price, price);
+
+                if (PrefUtils.getDisplayMode(getApplicationContext())
+                        .equals(getApplicationContext().getString(R.string.pref_display_mode_absolute_key))) {
+                    views.setTextViewText(R.id.widget_price_change, change);
+
+                } else {
+                    views.setTextViewText(R.id.widget_price_change, percentage);
+                }
+
+
+
+                if (rawAbsoluteChange > 0) {
+
+                    views.setTextColor(R.id.widget_price_change, getResources().getColor(R.color.material_green_700));
+                } else {
+                    views.setTextColor(R.id.widget_price_change, getResources().getColor(R.color.material_red_700));
+                }
 
 
 
@@ -88,13 +154,12 @@ public class StockHawkRemoteViewsService extends RemoteViewsService{
 
             @Override
             public int getViewTypeCount() {
-                Timber.v("FACTORY getViewTypeCount");return 0;
+                Timber.v("FACTORY getViewTypeCount");return 1;
             }
 
             @Override
             public long getItemId(int position) {
-
-                Timber.v("getItemId");return 0;
+                Timber.v("getItemId");return position;
             }
 
             @Override
--- app/src/main/java/com/udacity/stockhawk/widget/StockHawkWidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/widget/StockHawkWidgetProvider.java
@@ -1,16 +1,20 @@
 package com.udacity.stockhawk.widget;
 
+import android.annotation.TargetApi;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
 import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.sync.QuoteSyncJob;
+import com.udacity.stockhawk.ui.DetailActivity;
 import com.udacity.stockhawk.ui.MainActivity;
 
 import timber.log.Timber;
@@ -35,20 +39,20 @@ public class StockHawkWidgetProvider extends AppWidgetProvider {
 
 
         for(int appWidgetId: appWidgetIds){
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+
 
             Intent launchAppIntent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchAppIntent, 0);
+            views.setOnClickPendingIntent(R.id.widget_holder, pendingIntent);
 
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
             views.setOnClickPendingIntent(R.id.widget_holder, pendingIntent);
             views.setEmptyView(R.id.widget_listview, R.id.widget_empty_view);
-            views.setOnClickPendingIntent(R.id.widget_holder, pendingIntent);
-            views.setRemoteAdapter(R.id.widget_listview, new Intent(context, RemoteViewsService.class));
-            if(mIsUpdated){
-                context.startService(new Intent(context, StockHawkWidgetIntentService.class));
+            views.setPendingIntentTemplate(R.id.widget_listview, pendingIntent);
 
-            }
 
+
+            views.setRemoteAdapter(R.id.widget_listview, new Intent(context, StockHawkRemoteViewsService.class));
             appWidgetManager.updateAppWidget(appWidgetId, views);
 
         }
@@ -61,20 +65,16 @@ public class StockHawkWidgetProvider extends AppWidgetProvider {
         super.onReceive(context, intent);
         Timber.v("Received intent:  " + intent.getAction());
 
-
-
-
         if(QuoteSyncJob.ACTION_DATA_UPDATED.equals(intent.getAction())){
 
             AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
             int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
-                    new ComponentName(context, getClass())
-            );
-            mIsUpdated = true;
-            onUpdate(context, appWidgetManager, appWidgetIds);
+                    new ComponentName(context, getClass()));
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_listview);
         }
 
 
     }
+
 
 }
